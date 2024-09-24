73dec7f8d4cb1cb311e44fb4a823194660309274
mai19950 <underdog-95@outlook.com>
2024-09-24 16:27:31 +0800

    feat: 更新主文件

--- scrape.py
+++ scrape.py
@@ -1,57 +1,59 @@
+import aiohttp
+import asyncio
+import aiofiles
 import os
-import requests
 from datetime import datetime
 
-def scrapy():
+# 从环境变量中获取 Telegram Bot Token 和 Chat ID
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+
+if not TELEGRAM_BOT_TOKEN or not TELEGRAM_CHAT_ID:
+  raise ValueError("TELEGRAM_BOT_TOKEN 和 TELEGRAM_CHAT_ID 环境变量未设置")
+
+
+async def scrape():
   # 获取当天日期，格式为 YYYYMMDD
   today = datetime.today().strftime('%Y%m%d')
 
   # 动态生成 URL，替换日期部分
   url = f'https://clashgithub.com/wp-content/uploads/rss/{today}.txt'
 
-  # 发送 GET 请求获取内容
-  response = requests.get(url)
-
-  # 检查请求是否成功
-  if response.status_code == 200:
-    content = response.text
-    
-    # 保存爬取到的内容
-    with open('site', 'w', encoding='utf-8') as file:
-      file.write(content)
-      
-    print("内容已成功保存")
-  else:
-    print(f"无法获取内容，状态码: {response.status_code}")
+  async with aiohttp.ClientSession() as session:
+    async with session.get(url) as response:
+      if response.status == 200:
+        content = await response.text()
+        async with aiofiles.open('site', 'w', encoding="utf-8") as file:
+          await file.write(content)
+        print("节点已成功保存")
+        return True
+      else:
+        print(f"无法获取内容，状态码：{response.status}")
+        return False
 
 
-def tg_notify():
-  # 从环境变量获取 Telegram Bot Token 和 Chat ID
-  telegram_bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
-  telegram_chat_id = os.getenv('TELEGRAM_CHAT_ID')
-
-  # 生成要发送的消息
-  current_date = datetime.now().strftime('%Y-%m-%d')
-  message = f"Scraping completed successfully on {current_date}"
-
-  # Telegram API URL
-  telegram_url = f"https://api.telegram.org/bot{telegram_bot_token}/sendMessage"
-
-  # 请求参数
+async def send_telegram_message(message):
+  url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
   payload = {
-      'chat_id': telegram_chat_id,
-      'text': message
+        'chat_id': TELEGRAM_CHAT_ID,
+        'text': message
   }
-  # 发送 POST 请求到 Telegram API
-  response = requests.post(telegram_url, data=payload)
+  async with aiohttp.ClientSession() as session:
+    async with session.post(url, json=payload) as response:
+      if response.status == 200:
+        print("Message sent successfully!")
+      else:
+        print(f"Failed to send message. Error: {response.status}, {await response.text()}")
 
-  # 检查请求是否成功
-  if response.status_code == 200:
-    print("Message sent successfully!")
-  else:
-    print(f"Failed to send message. Error: {response.status_code}, {response.text}")
+
+async def main():
+  scrape_success = await scrape()
+  
+  if scrape_success:
+    current_date = datetime.now().strftime('%Y-%m-%d')
+    message = f"Scraping completed successfully on {current_date}"
+    await send_telegram_message(message)
 
 
 if __name__ == '__main__':
-  scrapy()
-  tg_notify()
+  asyncio.run(main())
